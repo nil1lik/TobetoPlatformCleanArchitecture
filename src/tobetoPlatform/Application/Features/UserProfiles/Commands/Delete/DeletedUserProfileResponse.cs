97f53900dfa3f831fee435ec7c2d396d1a7d1eec
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-04 12:52:48 +0300

    Experience ve UserProfile silindi. Migrationdan sonra eklenecek

--- src/tobetoPlatform/Application/Features/UserProfiles/Commands/Delete/DeletedUserProfileResponse.cs
+++
@@ -1,8 +0,0 @@
-using Core.Application.Responses;
-
-namespace Application.Features.UserProfiles.Commands.Delete;
-
-public class DeletedUserProfileResponse : IResponse
-{
-    public int Id { get; set; }
-}
--- src/tobetoPlatform/Application/Features/UserProfiles/Constants/UserProfilesBusinessMessages.cs
+++
@@ -1,6 +0,0 @@
-namespace Application.Features.UserProfiles.Constants;
-
-public static class UserProfilesBusinessMessages
-{
-    public const string UserProfileNotExists = "User profile not exists.";
-}
