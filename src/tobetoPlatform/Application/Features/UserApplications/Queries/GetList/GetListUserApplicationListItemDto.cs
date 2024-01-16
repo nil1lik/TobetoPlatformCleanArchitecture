871fe959add14955dcc59cab537a09badb941074
Nil <144815514+nil1lik@users.noreply.github.com>
2024-01-16 09:36:39 +0300

    UserApplication Dto'su yapıldı.

--- src/tobetoPlatform/Application/Features/UserApplications/Queries/GetList/GetListUserApplicationListItemDto.cs
+++ src/tobetoPlatform/Application/Features/UserApplications/Queries/GetList/GetListUserApplicationListItemDto.cs
@@ -6,5 +6,6 @@ public class GetListUserApplicationListItemDto : IDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public List<string> ApplicationStepName { get; set; }
 
 }
