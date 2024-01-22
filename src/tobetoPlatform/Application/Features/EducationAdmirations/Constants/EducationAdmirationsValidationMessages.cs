05522c96f36ff93ea18fe9913a358dc87e5fdf05
Nil <144815514+nil1lik@users.noreply.github.com>
2024-01-22 15:54:39 +0300

    EducationAdmirationValidationMessage added.

---
+++ src/tobetoPlatform/Application/Features/EducationAdmirations/Constants/EducationAdmirationsValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.EducationAdmirations.Constants;
+public class EducationAdmirationsValidationMessages
+{
+    public const string AddedToFavourite = "Favorilere ekleme işlemin başarıyla gerçekleşti.";
+    public const string DeletedFromFavourite = "Favorilerden çıkarma işlemin başarıyla gerçekleşti.";
+}
